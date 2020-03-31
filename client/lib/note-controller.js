ce523a1f333124206a5e76eb9438753f57cbaf22
WilliamJGrace <williamjgrace@outlook.com>
2020-03-31 17:30:27 +0100

    note controller set up

---
+++ client/lib/note-controller.js
@@ -0,0 +1,13 @@
+function Notecontroller(noteList){
+  this.noteList = noteList
+  this.noteListView = new Notelistview(noteList);
+};
+
+Notecontroller.prototype.outputToHTML = function () {
+  document.getElementById('app').innerHTML = this.noteListView.showList()
+
+};
+let notelist = new Notelist();
+notelist.createNote("Hi there")
+let notecontroller = new Notecontroller(notelist);
+notecontroller.outputToHTML();
