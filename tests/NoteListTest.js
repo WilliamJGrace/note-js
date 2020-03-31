fefe98e7f2e0c1dee7667cdf54b8f37491203dc6
WilliamJGrace <williamjgrace@outlook.com>
2020-03-31 14:23:13 +0100

    Updated test

--- tests/NoteListTest.js
+++ tests/NoteListTest.js
@@ -5,18 +5,21 @@
 // })()
 "use strict";
 import { describe, it, expect, matchers } from './test_framework.js';
-var note = new Note("I'm a Note");
+var note = new Note("a note");
+var note2 = new Note("another note");
 var notelist = new Notelist();
 
 
 describe("NoteList", () => {
   describe("#createNotes", () =>{
       it("creates and stores a note", () =>{
-        notelist.createNote("a note")
-        notelist.createNote("another note")
+        notelist.createNote(note.getNote())
+        notelist.createNote(note2.getNote())
 
 
         expect(notelist.viewNotes()).arrayToBe(["a note", "another note"])
       })
   })
+
+
 })
