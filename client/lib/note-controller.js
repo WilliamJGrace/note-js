b13c1bc328c7400ed0b96cfb44455aeeaf73054e
WilliamJGrace <williamjgrace@outlook.com>
2020-04-03 14:36:28 +0100

    User can add a note in index

--- client/lib/note-controller.js
+++ client/lib/note-controller.js
@@ -11,11 +11,27 @@ Notecontroller.prototype.outputToHTML = function () {
 Notecontroller.prototype.hashChange = function () {
   window.addEventListener("hashchange",
   (location) => {
+    // event.preventDefault()
     var id = window.location.hash.split("notes/")[1]
     var singlenoteview = new Singlenoteview(this.noteList.noteList[id])
     document.getElementById('app').innerHTML = singlenoteview.showNote();
       })
     }
+
+Notecontroller.prototype.textFormSubmit = function () {
+  document.getElementById("text").addEventListener("submit", () => {
+      text = document.getElementById('input').value
+      console.log(this.noteList)
+      this.noteList.createNote(text)
+      this.noteListView = new Notelistview(this.noteList);
+      console.log(this.noteListView)
+      console.log(this.noteListView.showList())
+
+      this.outputToHTML();
+      event.preventDefault()
+
+    });
+};
 
 
 
@@ -30,3 +46,4 @@ let notecontroller = new Notecontroller(notelist);
 
 notecontroller.outputToHTML();
 notecontroller.hashChange();
+notecontroller.textFormSubmit();
